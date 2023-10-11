cce449dc7cbc40253fdf42138bd2c05911092e63
Souleymane DIALLO <diallo.souleymane1@uam.edu.sn>
2023-10-11 19:55:24 +0000

    Réagir aux évènements du DOM - event binding : ajout de notre premier boutton

--- src/app/snap-face/snap-face.component.ts
+++ src/app/snap-face/snap-face.component.ts
@@ -11,6 +11,7 @@ export class SnapFaceComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  buttonText!: string;
 
   ngOnInit(): void {
       this.title = 'La poupée de ma grand-mère';
@@ -18,6 +19,17 @@ export class SnapFaceComponent implements OnInit {
       this.createdDate = new Date();
       this.snaps = 6;
       this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+      this.buttonText = 'Oh Snap!';
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!'){
+      this.snaps ++;
+      this.buttonText = 'Oops, unSnap!';
+    } else{
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
   }
 
 }
