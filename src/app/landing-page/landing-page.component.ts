eac1690f34c0941dd30ffec577f29627b1619505
Souleymane DIALLO <diallo.souleymane1@uam.edu.sn>
2023-10-12 22:52:51 +0000

    Implémentation de la navigation par URL à travers le router

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
+  // Injection du router
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+      
+  }
+
+  // methode de navigation par url à travers le routeur : 
+  onContinue(): void {
+    this.router.navigateByUrl('facesnaps');
+  }
 
 }
