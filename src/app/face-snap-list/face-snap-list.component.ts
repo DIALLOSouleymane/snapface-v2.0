c6f0094b1089466b7ea76d321e10ca6742a63c81
Souleymane DIALLO <diallo.souleymane1@uam.edu.sn>
2023-10-12 18:50:52 +0000

    Création du service pour les faceSnaps et injection des dépendances

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,65 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 
+import { FaceSnapService } from '../services/face-snaps.service';
+
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
 export class FaceSnapListComponent implements OnInit{
+  constructor(private faceSnapsService: FaceSnapService) { }
+
   faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.faceSnaps = [
-      {
-        title: 'La poupée de ma grand-mère',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Dakar'
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les rendonnées',
-        imageUrl: '../assets/White_Rock_Mountain.jpg',
-        createdDate: new Date(),
-        snaps: 200,
-        location: 'Paris'
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh, Que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 19
-  
-      },
-      {
-        title: 'La poupée de ma grand-mère',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Dakar'
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les rendonnées',
-        imageUrl: '../assets/White_Rock_Mountain.jpg',
-        createdDate: new Date(),
-        snaps: 200,
-        location: 'Paris'
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh, Que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 19
-  
-      }
-    ];
+    this.faceSnaps = this.faceSnapsService.faceSnaps;
   }
 
 }
---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@angular/core";
+import { FaceSnap } from "../models/face-snap.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class FaceSnapService {
+    faceSnaps: FaceSnap[] = [
+        {
+          title: 'La poupée de ma grand-mère',
+          description: 'Mon meilleur ami depuis tout petit !',
+          imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+          createdDate: new Date(),
+          snaps: 0,
+          location: 'Dakar'
+        },
+        {
+          title: 'Three Rock Mountain',
+          description: 'Un endroit magnifique pour les rendonnées',
+          imageUrl: '../assets/White_Rock_Mountain.jpg',
+          createdDate: new Date(),
+          snaps: 200,
+          location: 'Paris'
+        },
+        {
+          title: 'Un bon repas',
+          description: 'Mmmh, Que c\'est bon !',
+          imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+          createdDate: new Date(),
+          snaps: 19
+    
+        },
+        {
+          title: 'La poupée de ma grand-mère',
+          description: 'Mon meilleur ami depuis tout petit !',
+          imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+          createdDate: new Date(),
+          snaps: 0,
+          location: 'Dakar'
+        },
+        {
+          title: 'Three Rock Mountain',
+          description: 'Un endroit magnifique pour les rendonnées',
+          imageUrl: '../assets/White_Rock_Mountain.jpg',
+          createdDate: new Date(),
+          snaps: 200,
+          location: 'Paris'
+        },
+        {
+          title: 'Un bon repas',
+          description: 'Mmmh, Que c\'est bon !',
+          imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+          createdDate: new Date(),
+          snaps: 19
+    
+        }
+      ];
+}
