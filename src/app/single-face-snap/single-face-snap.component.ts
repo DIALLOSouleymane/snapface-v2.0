82f2cbc4b2786a08d44f2dd4911fcc96600134b6
Souleymane DIALLO <diallo.souleymane1@uam.edu.sn>
2023-10-13 00:40:55 +0000

    Visualisation d'un seul FaceSnap

---
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapService } from '../services/face-snaps.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-single-face-snap',
+  templateUrl: './single-face-snap.component.html',
+  styleUrls: ['./single-face-snap.component.scss']
+})
+export class SingleFaceSnapComponent {
+  // Injection de FaceSnapService afin d'utiliser ses méthodes
+  constructor(private faceSnapsService: FaceSnapService,
+    private route: ActivatedRoute) { }
+  // Création d'un objet injectable
+  faceSnap!: FaceSnap;
+  buttonText!: string;
+
+  ngOnInit(): void {
+      this.buttonText = 'Oh Snap!';
+      const snapId = +this.route.snapshot.params['id'];
+      this.faceSnap = this.faceSnapsService.getFaceSnapById(snapId);
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!'){
+      // this.faceSnap.snaps ++;
+      // Utilisation du service
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+      this.buttonText = 'Oops, unSnap!';
+    } else{
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap')
+      this.buttonText = 'Oh Snap!';
+    }
+  }
+}
